411dadc940150ee5ce9446e7ba53de2cda783a63
Santiago Suesca <dssuescav@correo.udistrital.edu.co>
2022-11-02 22:38:15 -0500

    version 0.0.1

---
+++ Persona.py
@@ -0,0 +1,36 @@
+class Persona:
+    def __init__(self):
+        self.nombre=""
+        self.edad=int
+        self.genero=""
+        self.volumen=""
+        self.bigote=bool
+        
+        
+    def gritar(self):
+        pass
+    def bailar(self):
+        pass
+    def saltar(self):
+        pass
+    
+sujeto_a=Persona()
+sujeto_a.nombre="Camilo"
+sujeto_a.edad=20
+sujeto_a.genero="Pansexual"
+sujeto_a.volumen="Alto"
+sujeto_a.bigote=False
+ 
+sujeto_b=Persona()
+sujeto_b.nombre="Cristian"
+sujeto_b.edad=56
+sujeto_b.genero="Masculino"
+sujeto_b.volumen="medio"
+sujeto_b.bigote=False
+ 
+sujeto_c=Persona()   
+sujeto_a.nombre="Lina"
+sujeto_a.edad=19
+sujeto_a.genero="Femenino"
+sujeto_a.volumen="Bajo"
+sujeto_a.bigote=False
